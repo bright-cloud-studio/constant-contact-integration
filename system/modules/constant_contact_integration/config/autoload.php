6e82c8b790af979a6fd8f7df1afb85efdde49adc
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-22 14:44:12 -0500

    Create autoload.php

---
+++ system/modules/constant_contact_integration/config/autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Constant Contact Integration
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/constant-contact-integration
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Register Classes */
+ClassLoader::addClasses(array
+(
+  //'KeapIntegration\Handler' => 'system/modules/keap_integration/src/Handler.php',
+  //'KeapIntegration\EventListener' => 'system/modules/keap_integration/src/EventListener.php'
+));
