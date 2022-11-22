7703f9313e7a3bec854a89f8e7de2fb3f968329d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-22 14:51:20 -0500

    Update config.php

--- system/modules/constant_contact_integration/config/config.php
+++ system/modules/constant_contact_integration/config/config.php
@@ -9,10 +9,3 @@
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
 **/
-
-/* Hooks */
-//$GLOBALS['TL_HOOKS']['createNewUser'][]      = array('KeapIntegration\Handler', 'newUserCreated');
-//$GLOBALS['TL_HOOKS']['activateAccount'][]    = array('KeapIntegration\Handler', 'accountActivated');
-
-/* Crons */
-//$GLOBALS['TL_CRON']['minutely'][] = ['KeapIntegration\EventListener', 'refreshKeapToken'];
