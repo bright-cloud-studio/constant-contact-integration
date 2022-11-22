626890648e32dd1abd0701217987a2883fd1a2f7
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-22 15:55:31 -0500

    Create EventListener.php

---
+++ system/modules/constant_contact_integration/src/EventListener.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ConstantContactIntegration;
+
+use Contao\System;
+
+class EventListener extends System
+{
+    public function refreshToken(): void
+    {
+       
+        // Testing the controller log
+        \Controller::log('CCI: Attempting token refresh', __CLASS__ . '::' . __FUNCTION__, 'GENERAL');
+
+        // Do stuffs here
+
+         // Testing the controller log
+        \Controller::log('CCI: Token refreshed - ' . $tokenVariable . '.', __CLASS__ . '::' . __FUNCTION__, 'GENERAL');
+    }
+}
