1331b68aa9f9c8ccd1f151c4f65412deff807799
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-22 14:44:42 -0500

    Create config.php

---
+++ system/modules/constant_contact_integration/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Constant Contact Integration
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/constant-contact-integration
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Hooks */
+//$GLOBALS['TL_HOOKS']['createNewUser'][]      = array('KeapIntegration\Handler', 'newUserCreated');
+//$GLOBALS['TL_HOOKS']['activateAccount'][]    = array('KeapIntegration\Handler', 'accountActivated');
+
+/* Crons */
+//$GLOBALS['TL_CRON']['minutely'][] = ['KeapIntegration\EventListener', 'refreshKeapToken'];
