865f126b725e04410a67f3183e2bf1852a31aa70
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-23 14:48:14 -0500

    Update EventListener.php

--- system/modules/constant_contact_integration/src/EventListener.php
+++ system/modules/constant_contact_integration/src/EventListener.php
@@ -13,6 +13,17 @@ class EventListener extends System
         \Controller::log('CCI: Attempting token refresh', __CLASS__ . '::' . __FUNCTION__, 'GENERAL');
 
         // Do stuffs here
+        $client = new \PHPFUI\ConstantContact\Client($apiKey, $secret, $tokenURL);
+        
+        $client = new \PHPFUI\ConstantContact\Client($apiKey, $secret, $tokenURL);
+        
+        
+        \header('location: ' . $client->getAuthorizationURL());
+        
+        $client->aquireAccessToken($_GET)
+        // Save these in DB
+        //$client->accessToken;
+        //$client->refreshToken;
 
          // Testing the controller log
         //\Controller::log('CCI: Token refreshed - ' . $tokenVariable . '.', __CLASS__ . '::' . __FUNCTION__, 'GENERAL');
