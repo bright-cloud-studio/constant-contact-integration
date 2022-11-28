42df76a9af6d01901ec47b580312f367c7f17ece
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-28 14:00:02 -0500

    Create tl_module.php

---
+++ system/modules/constant_contact_integration/dca/tl_module.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Constant Contact Integration
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/constant-contact-integration
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Add a palette to tl_module */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['constant_contact_authorize'] 		= '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
