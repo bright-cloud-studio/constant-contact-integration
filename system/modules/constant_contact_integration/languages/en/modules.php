3aebffe3090e9412db262d2630a14e1824a22b13
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-28 13:33:14 -0500

    Create modules.php

---
+++ system/modules/constant_contact_integration/languages/en/modules.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Constant Contact Integration
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/constant-contact-integration
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Front end modules */
+$GLOBALS['TL_LANG']['FMD']['constant_contact_authorize']    = array('Constant Contact - Authorize', 'Use to authorize connection with Constant Contact');
