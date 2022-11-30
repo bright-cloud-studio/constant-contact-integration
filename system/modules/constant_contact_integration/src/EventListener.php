0f17b06de465cc445e6ad040bbe43a55e0297c77
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-30 10:59:33 -0500

    Update EventListener.php

--- system/modules/constant_contact_integration/src/EventListener.php
+++ system/modules/constant_contact_integration/src/EventListener.php
@@ -11,18 +11,34 @@ class EventListener extends System
        
         // Testing the controller log
         \Controller::log('CCI: Attempting token refresh', __CLASS__ . '::' . __FUNCTION__, 'GENERAL');
-
-        // Do stuffs here
-        $client = new \PHPFUI\ConstantContact\Client($apiKey, $secret, $tokenURL);
         
-        \header('location: ' . $client->getAuthorizationURL());
         
-        $client->aquireAccessToken($_GET);
-        // Save these in DB
-        //$client->accessToken;
-        //$client->refreshToken;
+        // get accessToken
+        $token_access = file_get_contents('token_access.txt');
+        
+        // get refreshToken
+        $token_refresh = file_get_contents('token_refresh.txt');
+        
+        
+        
+        
+        // refresh our tokens
+        $client = new \PHPFUI\ConstantContact\Client($apiKey, $secret, $redirectURI);
+        $client->accessToken = $token_access;
+        $client->refreshToken = $token_refresh;
+        $client->refreshToken();3
+        
+        
+        // save refreshed tokens
+        $file_handle = fopen('token_access.txt', 'w');
+		fwrite($file_handle, $client->accessToken);
+        
+        $file_handle = fopen('token_refresh.txt', 'w');
+		fwrite($file_handle, $client->refreshToken);
+        
 
          // Testing the controller log
-        //\Controller::log('CCI: Token refreshed - ' . $tokenVariable . '.', __CLASS__ . '::' . __FUNCTION__, 'GENERAL');
+        \Controller::log('CCI: Token refreshed - ' . $client->refreshToken . '.', __CLASS__ . '::' . __FUNCTION__, 'GENERAL');
+        
     }
 }
