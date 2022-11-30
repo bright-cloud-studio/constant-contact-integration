45cf55391fe72013b273aa6481a7bae37c5e4c4a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-30 11:46:24 -0500

    Create tl_form.php

---
+++ system/modules/constant_contact_integration/languages/en/tl_form.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Add News Fields
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/add-news-fields
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_form']['cci_legend'] = 'Constant Contact Integration Settings';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_form']['cci_list']          = array('Contact List', 'Select which list to associate with this form');
