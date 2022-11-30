41020f6d318e59e766e873173885d84fb5b3b7b8
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-30 12:35:36 -0500

    Update ConstantContactBackend.php

--- system/modules/constant_contact_integration/library/Bcs/Backend/ConstantContactBackend.php
+++ system/modules/constant_contact_integration/library/Bcs/Backend/ConstantContactBackend.php
@@ -1,13 +1,12 @@
 <?php
 
 /**
- * Contao Directory - Users can make submissions to a directory with a module to display and filter the results.
+ * Bright Cloud Studio's Constant Contact Integration
  *
- * Copyright (C) 2022 Bright Cloud Studio
- *
- * @package    bright-cloud-studio/contao-directory
- * @link       https://www.brightcloudstudio.com/
- * @license    http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright  2022-2023 Bright Cloud Studio
+ * @package    bright-cloud-studio/constant-contact-integration
+ * @license    GNU/LGPL
+ * @filesource
  */
 
  
@@ -19,9 +18,36 @@ class ConstantContactBackend extends \Backend
 {
     
     public function optionsLists() {
-		return array(
-            'USA'       => 'United States',
-            'Afghanistan'    => 'Afghanistan',
-            'Albania'    => 'Albania');
+
+         // Grab our stored tokens from the text files
+        $token_access = file_get_contents('token_access.txt');
+        $token_refresh = file_get_contents('token_refresh.txt');
+        
+        // Constant Contact API values
+        $redirectURI = 'https://framework.brightcloudstudioserver.com/cci_auth.php';
+        $apiKey = 'c58a64d0-f0b1-4ba0-a9c9-699d50d1df4c';
+        $secret = 'VuMhHswFzPHsUe4YtV7VtA';
+        
+        // establish our connection
+        $client = new \PHPFUI\ConstantContact\Client($apiKey, $secret, $redirectURI);
+        $client->accessToken = $token_access;
+        $client->refreshToken = $token_refresh;
+        
+        // get ours lists
+        $listEndPoint = new \PHPFUI\ConstantContact\V3\ContactLists($client);
+        $lists = $listEndPoint->get();
+        
+        // array to store our results until they are returned
+        $arrLists = [];
+        
+        foreach($lists['lists'] as $entry){
+            $arrLists[$entry['name']] = $entry['name'];
+        }
+        
+        // sort the array alphabetically ascending
+        asort($arrLists);
+        
+        // return our stored results
+		return $arrLists;
 	}
 }
