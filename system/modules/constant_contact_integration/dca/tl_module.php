d4ffde965351cf7df8fa04f679c0abde34991124
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-12-01 15:13:50 -0500

    Update tl_module.php

--- system/modules/constant_contact_integration/dca/tl_module.php
+++ system/modules/constant_contact_integration/dca/tl_module.php
@@ -11,4 +11,34 @@
 **/
 
 /* Add a palette to tl_module */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['constant_contact_authorize'] 		= '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['constant_contact_authorize'] 		= '{title_legend},name,headline,type;{cci_auth_legend},cci_key,cci_secret,cci_url;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
+
+// Sort Fields
+$GLOBALS['TL_DCA']['tl_module']['fields']['cci_key'] = array
+(
+	'label' 			    => &$GLOBALS['TL_LANG']['tl_module']['cci_key'],
+	'inputType' 		    => 'text',
+    'default'               => '',
+    'search'                => true,
+	'eval' 				    => array('mandatory'=>false, 'tl_class'=>'clr long'),
+	'sql' 				    => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_module']['fields']['cci_secret'] = array
+(
+	'label' 			    => &$GLOBALS['TL_LANG']['tl_module']['cci_secret'],
+	'inputType' 		    => 'text',
+    'default'               => '',
+    'search'                => true,
+	'eval' 				    => array('mandatory'=>false, 'tl_class'=>'clr long'),
+	'sql' 				    => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_module']['fields']['cci_url'] = array
+(
+	'label' 			    => &$GLOBALS['TL_LANG']['tl_module']['cci_url'],
+	'inputType' 		    => 'text',
+    'default'               => '',
+    'search'                => true,
+	'eval' 				    => array('mandatory'=>false, 'tl_class'=>'clr long'),
+	'sql' 				    => "varchar(255) NOT NULL default ''"
+);
+
