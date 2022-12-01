ec1bbdf9ac15557106e9089ac929c4d51654bb9e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-12-01 15:38:51 -0500

    Create tl_form_field.php

---
+++ system/modules/constant_contact_integration/languages/en/tl_form_field.php
@@ -0,0 +1,13 @@
+<?
+
+/**
+ * Bright Cloud Studio's Constant Contact Integration
+ *
+ * @copyright  2022-2023 Bright Cloud Studio
+ * @package    bright-cloud-studio/constant-contact-integration
+ * @license    GNU/LGPL
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['tl_form_field']['cci_linked_field_legend'] 	= "Constant Contact Integration - Link to Field";
+$GLOBALS['TL_LANG']['tl_form_field']['cci_linked_field']          = array('Linked Field', 'Select which Contact Field this data will be added to');
