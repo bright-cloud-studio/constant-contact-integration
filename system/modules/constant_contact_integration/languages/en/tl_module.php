9b66f0f2ce427867c9f2b52f586b4754053fc4dd
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-12-01 15:39:07 -0500

    Update tl_module.php

--- system/modules/constant_contact_integration/languages/en/tl_module.php
+++ system/modules/constant_contact_integration/languages/en/tl_module.php
@@ -4,12 +4,6 @@
 $GLOBALS['TL_LANG']['tl_module']['cci_legend'] 	= "Constant Contact Integration - Select Contact List";
 $GLOBALS['TL_LANG']['tl_module']['cci_list'] 		= array('Contact List', 'Select which Contact List this form will be submitting data to');
 
-
-$GLOBALS['TL_LANG']['tl_module']['cci_linked_field_legend'] 	= "Constant Contact Integration - Link to Field";
-$GLOBALS['TL_LANG']['tl_module']['cci_linked_field']          = array('Linked Field', 'Select which Contact Field this data will be added to');
-
-
-
 $GLOBALS['TL_LANG']['tl_module']['cci_auth_legend'] 	= "Constant Contact Integration - Enter API Details";
 $GLOBALS['TL_LANG']['tl_module']['cci_key']           = array('API Key', 'Enter your Constant Contact\'s application key');
 $GLOBALS['TL_LANG']['tl_module']['cci_secret']        = array('API Secret', 'Enter your Constant Contact\'s application secret');
