e4d48eaa29b299913b6f86921f5ac4e1c0da593c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-12-08 10:52:06 -0500

    Update Handler.php

--- system/modules/constant_contact_integration/library/Bcs/Handler.php
+++ system/modules/constant_contact_integration/library/Bcs/Handler.php
@@ -14,19 +14,8 @@ class Handler
         // if this form has a CC list associated with it
         if($formData['cci_list'] != '') {
             
-            // Grab our stored tokens from the text files
-            $token_access = file_get_contents('token_access.txt');
-            $token_refresh = file_get_contents('token_refresh.txt');
-            
-            // Constant Contact API values
-            $redirectURI = 'https://framework.brightcloudstudioserver.com/cci_auth.php';
-            $apiKey = 'c58a64d0-f0b1-4ba0-a9c9-699d50d1df4c';
-            $secret = 'VuMhHswFzPHsUe4YtV7VtA';
-            
-            // establish our connection
-            $client = new \PHPFUI\ConstantContact\Client($apiKey, $secret, $redirectURI);
-            $client->accessToken = $token_access;
-            $client->refreshToken = $token_refresh;
+            $has_params = 0;
+            $has_permission = 0;
             
             // set up our Constant Contact parameters
             $params = [];
@@ -40,15 +29,42 @@ class Handler
                 if($result->cci_linked_field != '') {
                     // set the linked field to the parameters we are going to pass to Constant Contact
                     $params[$result->cci_linked_field] = $submittedData[$result->name];
+                    $has_params = 1;
                 }
+                
+                // if this form has a checkbox with cci_express_permission checked
+                if($result->type == 'checkbox' && $result->cci_express_permission == 1) {
+                     // if this form field is actually checked
+                     if($submittedData[$result->name] != "")
+                        $has_permission = 1;
+                }
+                   
+            }
+            
+            if($has_params != 0 && $has_permission != 0) {
+                
+                // Grab our stored tokens from the text files
+                $token_access = file_get_contents('token_access.txt');
+                $token_refresh = file_get_contents('token_refresh.txt');
+                
+                // Constant Contact API values
+                $redirectURI = 'https://framework.brightcloudstudioserver.com/cci_auth.php';
+                $apiKey = 'c58a64d0-f0b1-4ba0-a9c9-699d50d1df4c';
+                $secret = 'VuMhHswFzPHsUe4YtV7VtA';
+                
+                // establish our connection
+                $client = new \PHPFUI\ConstantContact\Client($apiKey, $secret, $redirectURI);
+                $client->accessToken = $token_access;
+                $client->refreshToken = $token_refresh;
+                
+                // Create a Contact object using the details from above
+                $contact = new \PHPFUI\ConstantContact\Definition\ContactCreateOrUpdateInput($params);
+                
+                // Attempt to "signup" this new Contact
+                $signup = new \PHPFUI\ConstantContact\V3\Contacts\SignUpForm($client);
+                $response = $signup->post($contact);
             }
 
-            // Create a Contact object using the details from above
-            $contact = new \PHPFUI\ConstantContact\Definition\ContactCreateOrUpdateInput($params);
-            
-            // Attempt to "signup" this new Contact
-            $signup = new \PHPFUI\ConstantContact\V3\Contacts\SignUpForm($client);
-            $response = $signup->post($contact);
         }
         
     }
