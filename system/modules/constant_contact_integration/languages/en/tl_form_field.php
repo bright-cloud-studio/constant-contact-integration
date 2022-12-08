113de87b98087c3c3d0861d5c37efe79daae1d29
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-12-08 10:51:39 -0500

    Update tl_form_field.php

--- system/modules/constant_contact_integration/languages/en/tl_form_field.php
+++ system/modules/constant_contact_integration/languages/en/tl_form_field.php
@@ -11,3 +11,6 @@
 
 $GLOBALS['TL_LANG']['tl_form_field']['cci_linked_field_legend'] 	= "Constant Contact Integration - Link to Field";
 $GLOBALS['TL_LANG']['tl_form_field']['cci_linked_field']          = array('Linked Field', 'Select which Contact Field this data will be added to');
+
+$GLOBALS['TL_LANG']['tl_form_field']['cci_express_permission_legend'] 	= "Constant Contact Integration - Permission";
+$GLOBALS['TL_LANG']['tl_form_field']['cci_express_permission']          = array('Express Permission', 'The form will not submit to Constant Contact without this being checked');
