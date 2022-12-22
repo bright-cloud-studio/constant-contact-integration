7ad8ba17181d196835436933ffd834e3cbacceb3
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-12-22 11:56:08 -0500

    Update tl_form.php

--- system/modules/constant_contact_integration/dca/tl_form.php
+++ system/modules/constant_contact_integration/dca/tl_form.php
@@ -10,7 +10,7 @@
  */
 
  /* Extend the tl_news palettes */
-$GLOBALS['TL_DCA']['tl_form']['palettes']['default'] = str_replace(';{expert_legend:hide}', ';{cci_legend},cci_list;{expert_legend:hide}', $GLOBALS['TL_DCA']['tl_form']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_form']['palettes']['default'] = str_replace(';{expert_legend:hide}', ';{cci_legend},cci_list;cci_implied;{expert_legend:hide}', $GLOBALS['TL_DCA']['tl_form']['palettes']['default']);
 
 /* Add fields to tl_user */
 $GLOBALS['TL_DCA']['tl_form']['fields']['cci_list'] = array
@@ -21,4 +21,12 @@ $GLOBALS['TL_DCA']['tl_form']['fields']['cci_list'] = array
     'options_callback'          => array('Bcs\Backend\ConstantContactBackend', 'optionsLists'),
     'eval'                      => array('includeBlankOption'=>false, 'mandatory'=>false, 'chosen'=>true, 'tl_class'=>'w50'),
     'sql'                       => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_form']['fields']['cci_implied'] = array
+(
+    'label'                     => &$GLOBALS['TL_LANG']['tl_form']['cci_implied'],
+    'inputType'                 => 'radio',
+    'options'                   => array('yes' => 'Yes', 'no' => 'No'),
+    'eval'                      => array('mandatory'=>true, 'tl_class'=>'w50'),
+    'sql'                       => "varchar(32) NOT NULL default ''"
 );
