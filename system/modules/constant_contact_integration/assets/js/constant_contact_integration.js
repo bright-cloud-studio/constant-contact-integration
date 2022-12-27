651568cec6a8bd9f5d1cedd5a6da54271e41c074
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-12-27 13:06:44 -0500

    Update constant_contact_integration.js

--- system/modules/constant_contact_integration/assets/js/constant_contact_integration.js
+++ system/modules/constant_contact_integration/assets/js/constant_contact_integration.js
@@ -1,10 +1,10 @@
     
     // Pass our $_GET values to the authorize script to establish the connection to the Constant Contact Oauth2 API
-    function finalizeConnection(code, state){
+    function finalizeConnection(code, state, key, secret, uri){
         $.ajax({
             url: '/system/modules/constant_contact_integration/assets/php/action.api.authorize.php',
             type: 'GET',
-            data:{ code: code, state: state},
+            data:{ code: code, state: state, key: key, secret: secret, uri: uri},
             success:function(result){
                 $(".message").html(result);
             },
